51203fec021ea788d47c60294e25b09b0f17ddb6
kwg <kwg@users.noreply.github.com>
2018-05-22 15:04:22 -0500

    Implement Basic Color Prefs
    #2
    
    Portals now recolor based on the selected portal. New color is a lerp of oldColor of selectedColor and uses a float factor (0-1, where 1 is all selectedColor, 0 is all oldColor)

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -3,22 +3,37 @@
 public class ColorChanger : MonoBehaviour
 {
     private Color color;
+    private float lerpFactor = 0.9f;
 
+    private void Start()
+    {
+        color = gameObject.GetComponent<Renderer>().material.color;
+    }
 
     public void SetColor(Color newColor)
     {
-        Renderer rend = gameObject.GetComponent<Renderer>();
         color = newColor;
-        rend.material.SetColor("_Color", color);
     }
 
-    public Color GetColor()
+    public Color GetCurrentColor()
     {
         return color;
     }
 
-    void Change()
+    public void EvolveColor(Color selectedColor)
     {
-        gameObject.GetComponent<Material>().SetColor("_Color", color);
+        foreach(Portal portal in FindObjectsOfType<Portal>())
+        {
+            Color oldColor = portal.GetComponent<ColorChanger>().GetCurrentColor();
+            Color newColor = Color.Lerp(selectedColor, oldColor, lerpFactor);
+            portal.GetComponent<ColorChanger>().SetColor(newColor);
+        }
+    }
+
+    void Update()
+    {
+        //        gameObject.GetComponent<Material>().SetColor("_Color", color);
+        Renderer rend = gameObject.GetComponent<Renderer>();
+        rend.material.SetColor("_Color", color);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,8 +8,11 @@ public class Player : MonoBehaviour {
     {
         if(collider.gameObject.tag == "portal")
         {
-            Color newColor = new Color(Random.value, Random.value, Random.value);
-            collider.gameObject.GetComponent<ColorChanger>().SetColor(newColor);
+            //            Color newColor = new Color(Random.value, Random.value, Random.value);
+            //            collider.gameObject.GetComponent<ColorChanger>().SetColor(newColor);
+            ColorChanger colChanger = collider.gameObject.GetComponent<ColorChanger>();
+            Color selectedColor = colChanger.GetCurrentColor();
+            colChanger.EvolveColor(selectedColor);
 
             Vector3 newDestination = collider.gameObject.GetComponent<Portal>().Destination;
 

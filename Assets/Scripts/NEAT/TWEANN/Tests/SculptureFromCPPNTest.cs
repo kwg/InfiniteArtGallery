561af9477c9772744a8f7be53e276fec99f73d09
Will Price <Squillyprice01@users.noreply.github.com>
2018-06-25 14:09:37 -0500

    SculptureFromCPPNTest now rotates the sculpture, fixed a typo in Sculptures.cs

--- Assets/Scripts/NEAT/TWEANN/Tests/SculptureFromCPPNTest.cs
+++ Assets/Scripts/NEAT/TWEANN/Tests/SculptureFromCPPNTest.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class SculptureFromCPPNTest : MonoBehaviour {
     public GameObject sculpture;
-	// Use this for initialization
+    private GameObject sculptureProp;
+    private Vector3 center;
+    private float ROTATION_SPEED = 40;
+    // Use this for initialization
 	void Start () {
-        GameObject sculptureProp = Instantiate(sculpture) as GameObject;
+        sculptureProp = Instantiate(sculpture) as GameObject;
         sculptureProp.AddComponent<Sculptures>();
+        sculptureProp.transform.position = new Vector3(0, 1, 0);
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        sculptureProp.transform.Rotate(0, ROTATION_SPEED * Time.deltaTime, 0);
+    }
 }

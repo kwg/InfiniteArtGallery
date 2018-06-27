777140c440c290cbd65bff9374dfe1eb63d35b3e
kwg <kwg@users.noreply.github.com>
2018-06-27 15:06:44 -0500

    Inventory/HUD rework - Function Slots added
    #33
    function tray added to hud
    
    hud/inventory reworked to be more dynamic

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -50,61 +50,6 @@ public class Player : MonoBehaviour {
     public void Update()
     {
         // FIXME Move all of this to inventory. pass the collider to inventory and have it figure out what to do next. this is getting messy and hard to debug
-        if(Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = new Ray(camera.transform.position, camera.transform.forward * interactionDistance);
-            if (Physics.Raycast(ray, out hit))
-            {
-                Transform objectHit = hit.transform;
-                if(hit.collider.tag == "portal")
-                {
-                    Portal p = hit.collider.gameObject.GetComponent<Portal>();
-                    Texture2D img = new Texture2D(256, 256, TextureFormat.ARGB32, false); // HACK hardcoded width and height
-                    Graphics.CopyTexture(p.GetImage(), img);
-                    int portalID = p.GetPortalID();
-                    TWEANNGenotype geno = ag.GetArtwork(portalID).GetGenotype().Copy();
-
-                    SavedArtwork newArtwork = new SavedArtwork
-                    {
-                        Image = Sprite.Create(img, new Rect(0, 0, img.width, img.height), new Vector2(0.5f, 0.5f)) as Sprite,
-                        Geno = geno
-
-                    };
-                    inventory.AddItem(newArtwork);
-                }
-            }
-        }
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            RaycastHit hit;
-            Ray ray = new Ray(camera.transform.position, camera.transform.forward * interactionDistance);
-            if (Physics.Raycast(ray, out hit))
-            {
-                Transform objectHit = hit.transform;
-                if (hit.collider.tag == "portal")
-                {
-                    Portal p = hit.collider.gameObject.GetComponent<Portal>();
-                    ag.GetArtwork(p.GetPortalID()).SetGenotypePortal(inventory.GetActiveSlotItem().Geno.Copy());
-                    ag.GetArtwork(p.GetPortalID()).GenerateImageFromCPPN();
-                    ag.GetArtwork(p.GetPortalID()).ApplyImageProcess();
-                }
-            }
-        }
-
-        float wheel = Input.GetAxis("Mouse ScrollWheel");
-        if(wheel < 0f )
-        {
-            //scroll down
-            inventory.CycleActiveSlot(-1);
-
-        }
-        else if(wheel > 0f)
-        {
-            //scroll up
-            inventory.CycleActiveSlot(1);
-
-        }
+        
     }
 }

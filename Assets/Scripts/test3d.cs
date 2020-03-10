1d220250b26aa77a2047d50defdea123f1f7c6c9
Bryan Hollingsworth <kwg@users.noreply.github.com>
2020-03-10 08:08:52 -0500

    transfering
    moving

--- Assets/Scripts/test3d.cs
+++ Assets/Scripts/test3d.cs
@@ -13,6 +13,10 @@ public class test3d : MonoBehaviour
         ActivationFunctions.ActivateFunction(FTYPE.TANH);
         SculpturePlatform s = sculptureObject.GetComponent<SculpturePlatform>();
         s.InitArtDisplay(new GeneticArt());
+        for(int i = 0; i < 10; i++)
+        {
+            s.GetGeneticArt().Mutate();
+        }
         //art.SetParentUnityObject(p);
         //s.UpdateGeneratedArt();
     }
